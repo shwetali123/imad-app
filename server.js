3b921cba678a6f652cc47488a422bb6899f5c7a5
shwetzzz <36707882+shwetali123@users.noreply.github.com>
2018-04-08 17:09:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get("/article_one",function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article_one'));
+ res.sendFile(path.join(__dirname, 'ui', '.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
